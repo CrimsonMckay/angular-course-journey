8fd39256121738430ece4fb9e0dda914b40691c0
JudeEdemNyahe <58336301+CrimsonMckay@users.noreply.github.com>
2024-09-07 16:05:20 +0000

    course(angular): @for directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
+import { COURSES } from './../../db-data';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CourseCardComponent } from './course-card/course-card.component';
-import { COURSES } from '../../db-data';
+
 import { Course } from './model/course';
 
 @Component({
@@ -12,9 +13,7 @@ import { Course } from './model/course';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  coreCourse = COURSES[0];
-  rjxCourse = COURSES[1];
-  ngrxCourse = COURSES[2];
+  courses = COURSES;
 
   onCourseSelected(course: Course) {
     console.log('App-component-triggered from card view', course);
