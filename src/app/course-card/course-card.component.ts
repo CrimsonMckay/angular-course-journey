e542e25d87aae418bc11e7fe2303baaccafa6b4a
JudeEdemNyahe <58336301+CrimsonMckay@users.noreply.github.com>
2024-09-14 13:31:57 +0000

    course(angular): ngClass directive

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Course } from '../model/course';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-course-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './course-card.component.html',
   styleUrl: './course-card.component.css',
 })
@@ -19,5 +20,11 @@ export class CourseCardComponent {
 
   onCourseViewed() {
     this.courseSelected.emit(this.course);
+  }
+  cardClasses() {
+    if (this.course.category === 'BEGINNER') {
+      return 'beginner';
+    }
+    return;
   }
 }
