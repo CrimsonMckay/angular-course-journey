075c46c309726232186ebf489bf46023d48c9747
JudeEdemNyahe <58336301+CrimsonMckay@users.noreply.github.com>
2024-09-15 13:27:34 +0000

    course(angular): angular pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,16 +4,22 @@ import { RouterOutlet } from '@angular/router';
 import { CourseCardComponent } from './course-card/course-card.component';
 
 import { Course } from './model/course';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CourseCardComponent],
+  imports: [RouterOutlet, CourseCardComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   courses = COURSES;
+  //learning pipes
+  title = COURSES[0].description;
+  price = 9.99;
+  startDate = new Date(2000, 0, 1);
+
 
   onCourseSelected(course: Course) {
     console.log('App-component-triggered from card view', course);
